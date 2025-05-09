e54d3983aecbea6a70f0a54c8b4f5f7b67431436
Christina Browne <112651196+ChrisBrowne01@users.noreply.github.com>
2025-05-09 09:00:51 +0100

    feat: Add job counter functionality with increment, decrement, and reset features

--- reactsample12/src/CreateJob.js
+++ reactsample12/src/CreateJob.js
@@ -12,11 +12,26 @@ const CreateJob = () => {
     console.log('Run job', jobCounter + 1); // Log the new job count to the console
   };
 
+  // This function will be called when the button is clicked
+  const handleSubEvent = () => {
+    // setJobCount is a function that decrements down the state variable jobCount
+    setJobCounter(jobCounter - 1);
+    console.log('Remove job', jobCounter - 1); // Log the new job count to the console
+  };
+
+  const handleResetEvent = () => {
+    // setJobCount is a function that resets the state variable jobCount
+    setJobCounter(0);
+    console.log('Reset job', 0); // Log the new job count to the console
+  }
+
   return (
     <div>
       <h1>Job Counter</h1>
       <p>Number of jobs : {jobCounter}</p>
-      <button onClick={handleClickEvent}>Run job</button>
+      <button onClick={handleClickEvent}>Run a job</button><br />
+      <button onClick={handleSubEvent}>Remove a job</button><br />
+      <button onClick={handleResetEvent}>Reset</button>
     </div>
   )
 }
